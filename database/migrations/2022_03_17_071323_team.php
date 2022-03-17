13b20cbffe191d6a1dd5781f48305c0826df6b40
sjibs <74805504+sjibs@users.noreply.github.com>
2022-03-17 07:17:32 +0000

    Generated Models, Migrations, and Factories
    Added models Migrations and Factories for the following classes:
    Reward, RewardCriteria, Team, TeamMetric

---
+++ database/migrations/2022_03_17_071323_team.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        schema::create('team', function (Blueprint $table) {
+            $table->unsignedBigInteger('psid');
+            $table->string('site',64);
+            $table->enum('qualifier', ['Good', 'Medium', 'Low']);
+            $table->double('ccpoh', 23, 20);
+            $table->double('art', 23, 20);
+            $table->double('nps', 23, 20);
+            $table->double('fcr', 23, 20);
+            $table->double('online_percentage', 23, 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
