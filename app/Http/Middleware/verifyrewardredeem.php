a0c42c0e9b4085f2590bbb33bfd2b9aeeb459679
sjibs <74805504+sjibs@users.noreply.github.com>
2022-04-20 15:21:14 +0100

    fixed teams filename
    capitalized the T in "teams.csv"

--- app/Http/Middleware/verifyrewardredeem.php
+++ app/Http/Middleware/verifyrewardredeem.php
@@ -21,10 +21,10 @@ class VerifyRewardRedeem
         $r = Reward::findOrFail($request->reward);
         $page = ($r->type=="skillbuilder")?"skillbuilderTab":"rewardsTab";
         if(Auth::user()->id != $r->psid){
-            return redirect('agentdashboard.show')->with('message','Something went wrong when redeeming your reward.')->with('page',$page);
+            return redirect('agentdashboard')->with('message','Something went wrong when redeeming your reward.')->with('page',$page);
         }
         if($r->redeemed){
-            return redirect('agentdashboard.show')->with('message','You have already redeemed this reward.')->with('page',$page);
+            return redirect('agentdashboard')->with('message','You have already redeemed this reward.')->with('page',$page);
         }
         return $next($request);
     }
