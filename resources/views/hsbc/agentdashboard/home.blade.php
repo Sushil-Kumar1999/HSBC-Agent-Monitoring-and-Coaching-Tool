72116276362678d774b5b5e5e4893af42df95f35
Srushti <srushtivasani@gmail.com>
2022-04-24 23:21:23 +0100

    Logout UI done - fix in route

--- resources/views/hsbc/agentdashboard/home.blade.php
+++ resources/views/hsbc/agentdashboard/home.blade.php
@@ -97,7 +97,7 @@
                     </div>
                     <div class="mt-3 space-y-1">
                         <!-- Authentication -->
-                        <form method="POST" action="http://127.0.0.1/logout">
+                        <form method="POST" action="{{ route('logout') }}">
                             <input type="hidden" name="_token" value="dA6hUZXf43T4RjxrQdvQUSnWfvKuOZVMjP9enwoX">
                             <a class="block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out" href="http://127.0.0.1/logout" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
