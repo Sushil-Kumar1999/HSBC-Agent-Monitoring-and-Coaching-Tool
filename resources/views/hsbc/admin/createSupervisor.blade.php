c3e2fa1796e35133e1b9e0ab5815084fd4d85248
Srushti <srushtivasani@gmail.com>
2022-04-25 14:52:35 +0100

    admin UI fix

--- resources/views/hsbc/admin/createSupervisor.blade.php
+++ resources/views/hsbc/admin/createSupervisor.blade.php
@@ -117,7 +117,7 @@
                 <div class="py-12">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" >
                         <div id="supervisor" class="p-3  overflow-hidden shadow-sm ">
-                            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                            <h2 class="font-semibold text-xl text-white text-800 leading-tight">
                                 {{ __('Create Supervisor') }}
                             </h2>
 
